65e65e498d695244a64be1918fd6abf93ff04727
gfriedtod <wilfried.wayou@2026.ucac-icam.com>
2025-03-10 14:37:14 +0100

    add messaging

--- src/main/java/com/api/guolo_api/adminMangement/application/in/LotterieUseCase.java
+++ src/main/java/com/api/guolo_api/adminMangement/application/in/LotterieUseCase.java
@@ -4,6 +4,7 @@ import com.api.guolo_api.adminMangement.domain.model.LotteryViewDto;
 import com.api.guolo_api.adminMangement.domain.model.LotterieDto;
 import com.api.guolo_api.adminMangement.domain.model.TicketDto;
 import com.api.guolo_api.adminMangement.domain.model.Winner;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import java.util.List;
 import java.util.UUID;
@@ -11,9 +12,9 @@ import java.util.UUID;
 public interface LotterieUseCase {
 
     public List<LotteryViewDto> fetchAll();
-    public LotterieDto save(LotterieDto lotterieDto);
+    public LotterieDto save(LotterieDto lotterieDto) throws JsonProcessingException;
     public LotterieDto update(LotterieDto lotterieDto);
     LotterieDto findById(UUID lotteryId);
     LotterieDto delete(LotterieDto lotterieDto);
-    public Winner draw(UUID lotteryId);
+    public Winner draw(UUID lotteryId) throws JsonProcessingException;
 }
